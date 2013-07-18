ef52f524a22392c3521cf7b645cd2af9c87d5d0f
Den Williams <work@dennis-williams.net>
2013-07-19 00:46:10 +1000

    Updates & test page
    Created new test page.
    Fixed a few issues.
    Wrapped in anonymous functions.

--- ko.extras.bootstrap.js
+++ ko.extras.bootstrap.js
@@ -1,49 +1,55 @@
-/**
- * Binding handler for a twitter bootstrap slider
- */
-ko.bindingHandlers.slider = {
-    init: function (element, valueAccessor, allBindingsAccessor) {
-        //initialize datepicker with some optional options
-        //var options = allBindingsAccessor().datepickerOptions || {};
-        $(element).slider().on("slideStop", function (ev) {
-            var observable = valueAccessor();
-            //console.log(JSON.stringify(ev));
-            observable(ev.value);
-        });
-    },
-    update: function (element, valueAccessor) {
-        var value = ko.utils.unwrapObservable(valueAccessor());
-        $(element).slider('setValue', value);
-        //.datepicker("setValue", value);
-    }
-};
-
-/**
- * Binding for a twitter bootstrap modal
- */
-ko.bindingHandlers.modal = {
-    init: function (element, valueAccessor, allBindingsAccessor) { 
-    	//console.log('--Binding modal--');
-    },
-    update: function (element, valueAccessor) {
-        var observable = valueAccessor();
-        var value = ko.utils.unwrapObservable(observable);
-        function onClose() {
-        	console.log('hiding modal');
-        	$(element).off('hidden', onClose);
-        	if (value) {
-        		observable(false);
-        	}
-        };
-        if (value) {
-        	console.log('showing modal');
-            $(element).modal('show').on('hidden', onClose);
-            // this is to focus input field inside dialog
-            $("input", element).focus();
-        }
-        else {
-        	console.log('hiding modal');
-            $(element).modal('hide');
-        }
-    }
-};
+(function(ko, $) {
+	if (typeof ko == 'undefined' || typeof $ == 'undefined')
+		return;
+	
+	/**
+	 * Binding handler for a twitter bootstrap slider
+	 */
+	ko.bindingHandlers.slider = {
+	    init: function (element, valueAccessor, allBindingsAccessor) {
+	        //initialize datepicker with some optional options
+	        //var options = allBindingsAccessor().datepickerOptions || {};
+	        $(element).slider().on("slideStop", function (ev) {
+	            var observable = valueAccessor();
+	            //console.log(JSON.stringify(ev));
+	            observable(ev.value);
+	        });
+	    },
+	    update: function (element, valueAccessor) {
+	        var value = ko.utils.unwrapObservable(valueAccessor());
+	        $(element).slider('setValue', value);
+	        //.datepicker("setValue", value);
+	    }
+	};
+	
+	/**
+	 * Binding for a twitter bootstrap modal
+	 */
+	ko.bindingHandlers.modal = {
+	    init: function (element, valueAccessor, allBindingsAccessor) { 
+	    	//console.log('--Binding modal--');
+	    },
+	    update: function (element, valueAccessor) {
+	        var observable = valueAccessor();
+	        var value = ko.utils.unwrapObservable(observable);
+	        function onClose() {
+	        	console.log('hiding modal');
+	        	$(element).off('hidden', onClose);
+	        	if (value) {
+	        		observable(false);
+	        	}
+	        };
+	        if (value) {
+	        	console.log('showing modal');
+	            $(element).modal('show').on('hidden', onClose);
+	            // this is to focus input field inside dialog
+	            $("input", element).focus();
+	        }
+	        else {
+	        	console.log('hiding modal');
+	            $(element).modal('hide');
+	        }
+	    }
+	};
+	
+})(ko, $);
--- ko.extras.js
+++ ko.extras.js
@@ -1,51 +1,92 @@
-/**
- * Pushes all values to an array, only firing an event after
- * all values have been added rather than each item.
- */
-ko.observableArray.fn.pushAll = function(valuesToPush) {
-    var underlyingArray = this();
-    this.valueWillMutate();
-    ko.utils.arrayPushAll(underlyingArray, valuesToPush);
-    this.valueHasMutated();
-    return this;
-};
+(function(ko, $) {
+	if (typeof ko == 'undefined' || typeof $ == 'undefined')
+		return;
+	
+	// ###################
+	// #### FUNCTIONS ####
+	// ###################
+	
+	/**
+	 * Pushes all values to an array, only firing an event after
+	 * all values have been added rather than each item.
+	 */
+	ko.observableArray.fn.pushAll = function(valuesToPush) {
+	    var underlyingArray = this();
+	    this.valueWillMutate();
+	    ko.utils.arrayPushAll(underlyingArray, valuesToPush);
+	    this.valueHasMutated();
+	    return this;
+	};
+	
+	/**
+	 * Replaces all the values in a knockout array with a new set
+	 */
+	ko.observableArray.fn.replaceAll = function(valuesToPush) {
+	    this.removeAll();
+	    this.pushAll(valuesToPush);
+	    return this;
+	    
+	    // would probably be just the same to go this(valuesToPush) ??
+	};
+	
+	// ###################
+	// #### EXTENDERS ####
+	// ###################	
+	
+	/**
+	 * Formats the numeric value to the specified number of decimal places.
+	 * to use - var myValue = ko.observable(1.223123).extend({numeric: 1});
+	 */
+	ko.extenders.numeric = function(target, precision) {
+	    var result = ko.dependentObservable({
+	        read: function() {
+	           return target().toFixed(precision); 
+	        },
+	        write: target 
+	    });
+	
+	    result.raw = target;
+	    return result;
+	};
 
-/**
- * Replaces all the values in a knockout array with a new set
- */
-ko.observableArray.fn.replaceAll = function(valuesToPush) {
-    this.removeAll();
-    this.pushAll(valuesToPush);
-    return this;
-};
+	// ##################
+	// #### BINDINGS ####
+	// ##################
 
-// to use - var myValue = ko.observable(1.223123).extend({numeric: 1});
-/**
- * Formats the numeric value to the specified number of decimal places
- */
-ko.extenders.numeric = function(target, precision) {
-    var result = ko.dependentObservable({
-        read: function() {
-           return target().toFixed(precision); 
-        },
-        write: target 
-    });
+	/**
+	 * numericText Knockout binding.
+	 * to use - data-bind="numericText: value, precision: 2"
+	 */
+	ko.bindingHandlers.numericText = {
+	    update: function(element, valueAccessor, allBindingsAccessor) {
+	       var value = ko.utils.unwrapObservable(valueAccessor()),
+	           precision = ko.utils.unwrapObservable(allBindingsAccessor().precision) || ko.bindingHandlers.numericText.defaultPrecision,
+	           formattedValue = value.toFixed(precision);
+	
+	        ko.bindingHandlers.text.update(element, function() { return formattedValue; });
+	    },
+	    defaultPrecision: 1  
+	};
 
-    result.raw = target;
-    return result;
-};
+	// #####################
+	// #### TRANSITIONS ####
+	// #####################
 
-//({ type: "numeric", precision: 2 }).
-/**
- * numericText Knockout binding
- */
-ko.bindingHandlers.numericText = {
-    update: function(element, valueAccessor, allBindingsAccessor) {
-       var value = ko.utils.unwrapObservable(valueAccessor()),
-           precision = ko.utils.unwrapObservable(allBindingsAccessor().precision) || ko.bindingHandlers.numericText.defaultPrecision,
-           formattedValue = value.toFixed(precision);
-
-        ko.bindingHandlers.text.update(element, function() { return formattedValue; });
-    },
-    defaultPrecision: 1  
-};
+	/**
+	 * Similar to the visible binding, except that it fades the element in and out.
+	 * Requires jQuery.
+	 * Note: this is taken direct from the Knockout website
+	 */
+	ko.bindingHandlers.fadeVisible = {
+	    init: function(element, valueAccessor) {
+	        // Initially set the element to be instantly visible/hidden depending on the value
+	        var value = valueAccessor();
+	        $(element).toggle(ko.utils.unwrapObservable(value)); // Use "unwrapObservable" so we can handle values that may or may not be observable
+	    },
+	    update: function(element, valueAccessor) {
+	        // Whenever the value subsequently changes, slowly fade the element in or out
+	        var value = valueAccessor();
+	        ko.utils.unwrapObservable(value) ? $(element).fadeIn() : $(element).fadeOut();
+	    }
+	};
+})(ko, $);
